67c496eb376c1b63c90b82728e203156a36c7aa0
kubincom <100620086+kubincom@users.noreply.github.com>
2022-03-22 20:08:51 +0100

    Add files via upload

---
+++ ukol3_sms.py
@@ -0,0 +1,18 @@
+def sms_rozesilka(cislo: int,):
+    if len(cislo) == 9:
+      return True
+    elif len(cislo) == 13:
+      if "+420" in cislo:
+        return True
+    else:
+      return False
+
+cislo = (input("Zadej číslo: "))
+cislo = cislo.replace(" ", "")
+if sms_rozesilka(cislo) == False:
+      print("špatné telefonní číslo")
+else:
+  zprava = (input("jakou zprávu chcete poslat? "))
+  cena = round(((len(zprava))//180+1)*3)
+  print (f"Cena sms je {cena} Kč.")
+
